a05d65dd129f321a6976470df2e59a384f72604b
Frantisek Varadzin <varadzin@gmail.com>
2022-11-06 13:00:08 +0100

    Initial Commit

---
+++ MyProtocolsExamples/MyProtocolsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProtocolsExamplesApp.swift
+//  MyProtocolsExamples
+//
+//  Created by Frantisek Varadzin on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MyProtocolsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
